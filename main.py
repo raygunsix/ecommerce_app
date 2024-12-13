d6e0698a687d9a97771f0ac5a70cbedf24723c94
Chris Reid <chris@raygun6.com>
2024-12-12 16:21:58 -0800

    generate receipt for purchased item

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from fpdf import FPDF
 
 class Item:
     def buy(self, item_id):
@@ -6,17 +7,38 @@ class Item:
         pass
 
 class Receipt:
-    def generate(self, item_id):
+    def __init__(self, item_id, item_name, item_price):
+        self.item_id = item_id
+        self.item_name = item_name
+        self.item_price = item_price
+
+        print(self.item_id)
+
+    def generate(self):
         """ Generates a reciept for an item """
-        pass
 
+        # Generate pdf    
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
 
-df = pd.read_csv("inventory.csv")
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{str(self.item_id)}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: {self.item_name}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: {str(self.item_price)}", ln=1)
+
+        pdf.output("receipt.pdf")
+
+df = pd.read_csv("inventory.csv", dtype=str)
 print(df)
 
 purchased_item = input("Choose an item to buy: ")
 item = Item()
 item.buy(purchased_item)
 
-receipt = Receipt()
-receipt.generate(purchased_item)
+row = df.loc[df["id"] == purchased_item, ["id", "name", "price"]].values[0]
+receipt = Receipt(row[0], row[1], row[2])
+receipt.generate()
