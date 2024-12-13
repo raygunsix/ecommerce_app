90fe17cd40dbf153d4e044f98a9dc40117f2e4fb
Chris Reid <chris@raygun6.com>
2024-12-12 16:45:53 -0800

    remove item from inventory when purchased

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import pandas as pd
 from fpdf import FPDF
 
 class Item:
-    def buy(self, item_id):
+    def __init__(self, item_id):
+        self.item_id = int(item_id)
+
+    def buy(self):
         """ Reduces the inventory quantity by one"""
-        pass
+        quantity = df.loc[df["id"] == self.item_id, "in stock"].squeeze()
+        quantity = quantity - 1
+        df.loc[df["id"] == self.item_id, "in stock"] = quantity
+        df.to_csv("inventory.csv", index=False)
 
 class Receipt:
     def __init__(self, item_id, item_name, item_price):
-        self.item_id = item_id
-        self.item_name = item_name
-        self.item_price = item_price
-
-        print(self.item_id)
+        self.item_id = str(item_id)
+        self.item_name = str(item_name)
+        self.item_price = str(item_price)
 
     def generate(self):
         """ Generates a reciept for an item """
@@ -22,23 +26,23 @@ class Receipt:
         pdf.add_page()
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Receipt nr.{str(self.item_id)}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.item_id}", ln=1)
 
         pdf.set_font(family="Times", size=16, style="B")
         pdf.cell(w=50, h=8, txt=f"Article: {self.item_name}", ln=1)
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Price: {str(self.item_price)}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Price: {self.item_price}", ln=1)
 
         pdf.output("receipt.pdf")
 
-df = pd.read_csv("inventory.csv", dtype=str)
+df = pd.read_csv("inventory.csv")
 print(df)
 
 purchased_item = input("Choose an item to buy: ")
-item = Item()
-item.buy(purchased_item)
+item = Item(purchased_item)
+item.buy()
 
-row = df.loc[df["id"] == purchased_item, ["id", "name", "price"]].values[0]
-receipt = Receipt(row[0], row[1], row[2])
+name, price = df.loc[df["id"] == 100, ["name", "price"]].squeeze()
+receipt = Receipt(purchased_item, name, price)
 receipt.generate()
